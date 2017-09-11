4ba555b563c75b27bfa69fad3b8fdbe61df4e19e
CONNELLY <connela@cocsce-l1d39-12.ds.sc.edu>
2017-09-11 00:04:13 -0400

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 /* 
- * File:   Assignment2_Connelly.cpp
+ * File: Assignment2_Connelly.cpp
  * Author: Andrew Connelly
- * Function: 
- * Input: Number then operation then Number
+ * Function: Perform several diffrent functions involving arrays such as finding the
+ * min, max, mean, median, and sorting of the array
+ * Input: The user will be prompted by the program asking them what size do the want the array, then they will fill the
+ * array's indexes with values up to the size of the array.
  * Output:  
  * Created on: 
  */
@@ -15,17 +17,62 @@ using namespace std;
 /*
  * 
  */
-
+double min(double myArray[], int size);
+double max(double myArray[], int size);
+double mean(double myArray[], int size);
+double median(double myArray[], int size);
+void sort(double myArray[], int size);
 void readArray(double myArray[], int size);
 void printArray(double myArray[], int size); 
 
 int  main(int argc, char** argv)
 {
-
+int size;
+double myArray[size];
+readArray(myArray[size], size);
+printArray(myArray[size], size);
+//sort();
+//min();
+//max();
+//mean();
+//median();
 return 0;
 }
 // This functions purpose is to find the min of the array
-void min(double myArray[], int size){
+double min(double myArray[], int size){
+
+ double min = myArray[0];
+   nclude <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ *
+ */
+double min(double myArray[], int size);
+double max(double myArray[], int size);
+double mean(double myArray[], int size);
+double median(double myArray[], int size);
+void sort(double myArray[], int size);
+void readArray(double myArray[], int size);
+void printArray(double myArray[], int size);
+
+int  main(int argc, char** argv)
+{
+int size;
+double myArray[size];
+readArray(myArray[size], size);
+printArray(myArray[size], size);
+//sort();
+//min();
+//max();
+//mean();
+//median();
+return 0;
+}
+// This functions purpose is to find the min of the array
+double min(double myArray[], int size){
 
  double min = myArray[0];
    for (int i = 0; i<size; i++){
@@ -34,9 +81,21 @@ void min(double myArray[], int size){
 }
 cout << "The min of the array is:"<< min <<endl;
 }
+//The purpose of the function is to find the max of the array by going through and searching through each index
+//and comparing them to the previous index and if it's greater it sets it as the max value.
+double max(double myArray[], int size){
+
+double max = myArray[0];
+   for (int i = 0; i<size; i++){
+                                                                  `for (int i = 0; i<size; i++){
+      if (myArray[i] < min)
+         min = myArray[i];
+}
+cout << "The min of the array is:"<< min <<endl;
+}
 //The purpose of the function is to find the max of the array by going through and searching through each index 
 //and comparing them to the previous index and if it's greater it sets it as the max value.
-void max(double myArray[], int size){
+double max(double myArray[], int size){
 
 double max = myArray[0];
    for (int i = 0; i<size; i++){
@@ -46,7 +105,7 @@ cout << "The max of the array is:"<< max <<endl;
 }
 }
 //The purpose of the function is to find the mean of the array by adding each index and dividing by the arrays size
-void mean(double myArray[], int size){
+double mean(double myArray[], int size){
 
 double sum = 0;
    
@@ -58,7 +117,7 @@ cout << "The mean of the array is:"<< mean <<endl;
 //The purpose of the function is to find the median of the array
 //It first checks to see if the size is even if so it then grabs the middle 2 values and adds them and divides it by 2 
 //If the size is odd the it simply divides it by 2 and looks at the index that equals
-void median(double myArray[], int size){
+double median(double myArray[], int size){
     double sum = 0;
     double median;
     if ( size% 2 == 0){
@@ -71,7 +130,7 @@ void median(double myArray[], int size){
     }
 cout << "The median of the array is:"<< median <<endl;
 }
-double  sort(double myArray[], int size){
+void sort(double myArray[], int size){
    for (int i =0; i<size; i++){
        for (int j = i+1; j<size; j++){
           if (myArray[i] > myArray[i]){
